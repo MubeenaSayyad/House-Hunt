6356d75d0ffa8c6d5b2d8993a25bac7f83887a34
Mubeena sayyad <sayyadmubeena248@gmail.com>
2026-02-20 22:21:45 +0530

    Add files via upload

---
+++ Project Files/backend/config/connect.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectionOfDb = () => {
+  console.log("Mongo URI:", process.env.MONGO_DB);
+
+  mongoose
+    .connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      throw new Error(`Could not connect to MongoDB: ${err}`);
+    });
+};
+
+module.exports = connectionOfDb;
